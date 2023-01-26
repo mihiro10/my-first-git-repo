9994f6df89fafec7a69cf2738a13a07f18003f92
Mihiro Suzuki <mihirosuzuki@utexas.edu>
2023-01-26 10:36:03 -0600

    hw 2 commit 1

---
+++ homework02/part1.py
@@ -0,0 +1,21 @@
+import json
+import random
+
+
+sites_dict = {"sites":[]}
+
+for i in range(5):
+    latitude = 16 + 2*random.random()
+    longitude = 82 + 2*random.random()
+    composition = random.choice(["stony","iron","stony-iron"])
+    id = i + 1
+    sites_dict["sites"].append({"site_id": id,
+                                "latitude": latitude, 
+                                "longitude": longitude,
+                                "composition": composition})
+
+print(sites_dict)
+with open("meteorite_sites.json", "w") as out:
+    json.dump(sites_dict, out, indent=2)
+
+
---
+++ working-with-json/import_tester.py
@@ -0,0 +1,4 @@
+import main_tester    # assumes it is in this directory, or installed in known location
+
+print(main_tester.check_hemisphere(35.0, 70.0))
+print(main_tester.check_hemisphere(-35.0, -70.0))
