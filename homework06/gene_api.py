7f6fe80e9173f3f72af43a6e5b9f18002f04e230
mihiro_js_vm <mihirosuzuki@utexas.edu>
2023-03-28 05:02:17 +0000

    redis change

--- homework06/gene_api.py
+++ homework06/gene_api.py
@@ -8,7 +8,7 @@ data = {}
 
 
 def get_redis_client():
-    return redis.Redis(host='redis-db', port=6379, db=0)
+    return redis.Redis(host='homework06_redis-db_1', port=6379, db=0)
 rd = get_redis_client()
 
 @app.route('/data', methods = ['POST', 'GET', 'DELETE'])
